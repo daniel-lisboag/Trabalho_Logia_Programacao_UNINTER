3afa323145dbd8fff3d74301206b8c1afd6fabb6
Daniel Lisboa <daniellisboag0@gmail.com>
2025-11-15 21:40:53 -0300

    Segunda parte do trabalho

--- trabalho_02.py
+++ trabalho_02.py
@@ -13,35 +13,35 @@ total = 0
 
 # Inicia a estrutura de repetição
 while True:
-   sabor = input('Entre com o sabor desejado (PS/PD): ') # Pergunta ao usuário o sabor dejesado
+   sabor = input('Entre com o sabor desejado (PS/PD): ') # Pergunta ao usuário o sabor desejado
    if sabor in ('PS', 'PD'):
-      tamanho = input('Entre com o tamanho desejado (P/M/G): ') # Pergunta ao usuário o tamanho dejesado
+      tamanho = input('Entre com o tamanho desejado (P/M/G): ') # Pergunta ao usuário o tamanho desejado
       if tamanho in ('P', 'M', 'G'): # Verifica tamanho pedido
          if sabor == 'PD': # Verifica sabor pedido
             if tamanho == 'P':
                total += 34
-               print('Você pedi uma Pizza Doce no tamanho P: R$34,00')
+               print('Você pediu uma Pizza Doce no tamanho P: R$34,00')
                print('')
             elif tamanho == 'M':
                total += 48
-               print('Você pedi uma Pizza Doce no tamanho M: R$48,00')
+               print('Você pediu uma Pizza Doce no tamanho M: R$48,00')
                print('')
             elif tamanho == 'G':
                total += 66
-               print('Você pedi uma Pizza Doce no tamanho G: R$66,00')
+               print('Você pediu uma Pizza Doce no tamanho G: R$66,00')
                print('')
          elif sabor == 'PS':
             if tamanho == 'P':
                total += 30
-               print('Você pedi uma Pizza Salgada no tamanho P: R$30,00')
+               print('Você pediu uma Pizza Salgada no tamanho P: R$30,00')
                print('')
             elif tamanho == 'M':
                total += 45
-               print('Você pedi uma Pizza Salgada no tamanho M: R$45,00')
+               print('Você pediu uma Pizza Salgada no tamanho M: R$45,00')
                print('')
             elif tamanho == 'G':
                total += 60
-               print('Você pedi uma Pizza Salgada no tamanho G: R$60,00')
+               print('Você pediu uma Pizza Salgada no tamanho G: R$60,00')
                print('')
          repetir = input('Deseja mais alguma coisa? (S/N): ') # Pergunta ao usuário se ele deseja mais algo
          if repetir == 'S':
@@ -53,7 +53,7 @@ while True:
             print(f'O valor total a ser pago: R${total:.2f}')
             break
       else:
-         print('Tamanho inválido. tente novamente') # Mensagem de erro caso o usuário digite tamanho errado
+         print('Tamanho inválido. Tente novamente') # Mensagem de erro caso o usuário digite tamanho errado
          print('')
    else:
       print('Sabor inválido. Tente novamente') # Mensagem de erro caso o usuário digite sabor errado
